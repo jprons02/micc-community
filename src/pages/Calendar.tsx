9d6708499d4d352cee5edb1a31d20b6f8b28dcde
Joseph Ronselli <joseph.ronselli@yahoo.com>
2023-08-03 14:34:48 -0400

    calendar functionality working

--- src/pages/Calendar.tsx
+++ src/pages/Calendar.tsx
@@ -1,28 +1,34 @@
 import React, { useEffect, useState } from 'react';
 import { googleCalendarAuthAPI } from '../services/APIs/googleCalendarAuth';
 import ReactCalendar from '../components/calendar/ReactCalendar';
+import { googleCalendarGetEventsAPI } from '../services/APIs/googleCalendarGetEvents';
+
+// Material UI
+import useMediaQuery from '@mui/material/useMediaQuery';
+import { useTheme } from '@mui/material/styles';
 
 const Calendar: React.FC = () => {
-  /*
+  const [events, setEvents] = useState<any[]>([]);
+
+  const theme = useTheme();
+  const isDesktop = useMediaQuery(theme.breakpoints.up('sm'));
+
   useEffect(() => {
-    getToken();
+    getGoogleCalendarEvents();
   }, []);
 
-  const [token, setToken] = useState('');
-
-  const getToken = async () => {
-    setToken(await googleCalendarAuthAPI());
+  const getGoogleCalendarEvents = async () => {
+    setEvents(await googleCalendarGetEventsAPI());
   };
-  */
 
   return (
     <div style={{ padding: '20px' }}>
-      {/*<GoogleCalendar token={token} />*/}
-      <ReactCalendar />
+      <h1 style={!isDesktop ? { fontSize: '20px' } : {}}>
+        Miccosukee Community Calendar
+      </h1>
+      <ReactCalendar events={events} />
     </div>
   );
 };
 
 export default Calendar;
-
-//<GoogleCalendar token={token} />
--- src/services/APIs/googleCalendarGetEvents.tsx
+++ src/services/APIs/googleCalendarGetEvents.tsx
@@ -1,28 +1,80 @@
+//Gets events from api in chunks until no more events or until 6 months from today.
+
 import { keys } from '../../data/keys';
 
-export const googleCalendarGetEventsAPI = async () => {
+const fetchEventsChunk = async (
+  timeMin: any,
+  chunkSize: number,
+  pageToken: any
+) => {
   const serviceKey = JSON.stringify(keys.google.calendarServiceAccountKey);
+  const response = await fetch(
+    'https://hzqen5ppl3.execute-api.us-east-1.amazonaws.com/prod/googleCalendarGetEvents',
+    {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'x-api-key': keys.lambdaGoogleCalendarGetEventsAPI,
+      },
+      body: JSON.stringify({
+        serviceKey,
+        timeMin,
+        maxResults: chunkSize,
+        pageToken,
+      }),
+    }
+  );
+  const data = await response.json();
+  return data;
+};
+
+export const googleCalendarGetEventsAPI = async () => {
+  const chunkSize = 20;
+  const today = new Date();
+  let allEvents: any = [];
+  let timeMin = today.toISOString();
+  const timeMax = new Date(
+    today.getFullYear(),
+    today.getMonth() + 6,
+    1
+  ).toISOString();
+  let pageToken = ''; // Initialize the pageToken as an empty string
 
   try {
-    const response = await fetch(
-      'https://hzqen5ppl3.execute-api.us-east-1.amazonaws.com/prod/googleCalendarGetEvents',
-      {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-          'x-api-key': keys.lambdaGoogleCalendarGetEventsAPI,
-        },
-        body: serviceKey,
+    while (timeMin <= timeMax) {
+      const data = await fetchEventsChunk(timeMin, chunkSize, pageToken); // Include the pageToken in the request
+      const events = data.events || [];
+
+      allEvents = allEvents.concat(events);
+
+      // No more events or nextPageToken is not available, break the loop.
+      if (events.length === 0 || !data.nextPageToken) {
+        return allEvents;
       }
-    );
-    const data = await response.json();
-    console.log('data from get events front end: ', data.events);
-    if (data.message !== 'An error occurred while retrieving events.') {
-      return data.events;
-    } else {
-      return 'Error in goolge get events api.';
+
+      // Update the pageToken for the next request
+      pageToken = data.nextPageToken;
+
+      // Get the latest event's start time
+      const latestEventStartTime = new Date(
+        events[events.length - 1].start.dateTime ||
+          events[events.length - 1].start.date
+      );
+
+      // Set timeMin to one millisecond after the latest event's start time
+      timeMin = new Date(latestEventStartTime.getTime() + 1).toISOString();
+
+      // Move timeMin to the start of the next month
+      timeMin = new Date(
+        latestEventStartTime.getFullYear(),
+        latestEventStartTime.getMonth() + 1,
+        1
+      ).toISOString();
     }
+
+    return allEvents;
   } catch (error) {
     console.log('error: ', error);
+    return 'Error in google get events api.';
   }
 };
